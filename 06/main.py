fd2c6df9b41bf1f7ac9174a4bb90b5634c8efff6
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2023-12-06 12:33:51 +0530

    Day: 06

--- 06/main.py
+++ 06/main.py
@@ -5,22 +5,7 @@ LINES = open(FILE, "r").read().split("\n")
 time = re.sub(" +", " ", LINES[0].split(":")[1]).strip().split(" ")
 dist = re.sub(" +", " ", LINES[1].split(":")[1]).strip().split(" ")
 
-def partOne(time, dist):
-    time = list(map(int, time))
-    dist = list(map(int, dist))
-    ans = 1
-    for t, d in zip(time, dist):
-        cnt = 0
-        for i in range(1, t + 1):
-            if (t - i) * i > d:
-                cnt += 1
-        ans *= cnt
-    return ans
-
-def partTwo(time, dist):
-    time = int("".join(time))
-    dist = int("".join(dist))
-
+def get_range(time, dist):
     low, high = 1, time
     while low <= high:
         mid = (low + high) // 2
@@ -38,6 +23,21 @@ def partTwo(time, dist):
         else:
             high = mid - 1
     e = time - low
+    return s, e
+
+def partOne(time, dist):
+    time = list(map(int, time))
+    dist = list(map(int, dist))
+    ans = 1
+    for t, d in zip(time, dist):
+        s, e = get_range(t, d)
+        ans *= (s - e)
+    return ans
+
+def partTwo(time, dist):
+    time = int("".join(time))
+    dist = int("".join(dist))
+    s, e = get_range(time, dist)
     return s - e
 
 if __name__ == "__main__":
