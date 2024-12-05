3ce2a7ebc9d8548ec2184591c5c9db6cf5a795af
pookie <vivekkolhe694@gmail.com>
2024-12-05 20:28:32 +0530

    Cleaned up

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -6,48 +6,37 @@ def process_input():
     lines = [list(map(int, line.split())) for line in open(FILE, "r")]
     return lines
 
-arr = process_input()
+lines = process_input()
 
-def part_one():
-    count = 0
-    seen = set()
-    for idx, item in enumerate(arr):
-        f1, f2 = True, True
-        for i in range(1, len(item)):
-            if not (item[i-1] < item[i] and abs(item[i-1] - item[i]) >= 1 and abs(item[i-1] - item[i]) <= 3):
-                f1 = False
-            if not (item[i-1] > item[i] and abs(item[i-1] - item[i]) >= 1 and abs(item[i-1] - item[i]) <= 3):
-                f2 = False
-        
-        if f1 or f2:
-            seen.add(idx)
-            count += 1
-    return seen, count
+def valid(arr):
+    f1, f2 = True, True
+    for i in range(1, len(arr)):
+        if not (arr[i-1] < arr[i] and abs(arr[i-1] - arr[i]) >= 1 and abs(arr[i-1] - arr[i]) <= 3):
+            f1 = False
+        if not (arr[i-1] > arr[i] and abs(arr[i-1] - arr[i]) >= 1 and abs(arr[i-1] - arr[i]) <= 3):
+            f2 = False
+    
+    if f1 or f2:
+        return True
+    return False
 
-def part_two():
-    seen, partOne = part_one()
-    count = 0
-    for idx, item in enumerate(arr):
-        if idx in seen:
+def both_parts():
+    c1, c2 = 0, 0
+    for item in lines:
+        if valid(item):
+            c1 += 1
             continue
 
         n = len(item)
         for i in range(n):
             temp = item.copy()
             temp.pop(i)
-            f1, f2 = True, True
-            for i in range(1, len(temp)):
-                if not (temp[i-1] < temp[i] and abs(temp[i-1] - temp[i]) >= 1 and abs(temp[i-1] - temp[i]) <= 3):
-                    f1 = False
-                if not (temp[i-1] > temp[i] and abs(temp[i-1] - temp[i]) >= 1 and abs(temp[i-1] - temp[i]) <= 3):
-                    f2 = False
-            
-            if f1 or f2:
-                count += 1
+            if valid(temp):
+                c2 += 1
                 break
-    return partOne, partOne + count
+    return c1, c1 + c2
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    print(part_two())
+    print(both_parts())
     print(f"Time taken: {(time.perf_counter() - start) * 1000} ms")
